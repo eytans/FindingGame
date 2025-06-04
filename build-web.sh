0ef15f669f156f81b9a32eb438e0c117dd080b45
Eytan Singher <eytan@singher.co.il>
2025-06-04 22:42:45 +0300

    Fixed GH pages (I think) and updated app name and description

--- build-web.sh
+++ build-web.sh
@@ -2,7 +2,7 @@
 
 # Build Flutter web app for GitHub Pages
 echo "Building Flutter web app..."
-flutter build web --release --output-dir="www"
+flutter build web --release --base-href="/WordBubbles/" --output-dir="www"
 
 echo "Build complete! Web app is available in the /www directory."
 echo "To test locally, you can serve the www directory with a local server."
